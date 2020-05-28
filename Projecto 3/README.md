e2bc5ddbfbfb74dbb808b4e9c74da9053ee7d0c6
JoaoLobarinhas <jlobarinhas@hotmail.com>
2020-05-29 00:23:33 +0100

    Projecto 3 - Beginning

---
+++ Projecto 3/README.md
@@ -0,0 +1,11 @@
+Libs são as bibliotecas que vamos mesmo utilizar.
+
+A outra pasta é só uma lib que deixei aqui caso falsa falta.
+
+Falta ainda libs para:
+
+Zeep - (isto é para fazer request com o SOAP, não encontrei nada parecido)
+base64(?) - não sei se não existe um já default
+pem
+OpenSSL - sei que existe
+Crypto 
---
+++ Projecto 3/cmd_config.cpp
@@ -0,0 +1,25 @@
+/* 
+------------------------------------------------------------------------------------------
+
+Ficheiro de configuração do URL do WSDL a utilizar e da APPLICATION_ID fornecida pela AMA.
+
+------------------------------------------------------------------------------------------
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// ApplicationId da entidade, fornecida pela AMA
+const string APPLICATION_ID = "Change to your ApplicationId";
+
+
+string main (){return APPLICATION_ID;}
+
+
+
+
+
+
+
